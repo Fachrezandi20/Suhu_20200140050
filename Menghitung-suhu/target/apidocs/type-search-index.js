c094b1810189858ef133066a562a33021dc6a323
Fachrezandi20 <96400132+Fachrezandi20@users.noreply.github.com>
2022-01-04 16:12:53 +0700

    Delete Menghitung-suhu directory

--- Menghitung-suhu/target/apidocs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"com.example.Menghitung.suhu","l":"CelciustoFarenheit"},{"p":"com.example.Menghitung.suhu","l":"konversiberat"},{"p":"com.example.Menghitung.suhu","l":"KonversiSuhu"},{"p":"com.example.Menghitung.suhu","l":"MenghitungSuhuApplication"}]
