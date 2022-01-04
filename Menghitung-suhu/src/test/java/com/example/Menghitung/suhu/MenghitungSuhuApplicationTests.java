a999473ebe8c5a1f8cb25f47a3768c2b8cdfda02
Fachrezandi20 <96400132+Fachrezandi20@users.noreply.github.com>
2022-01-04 16:18:16 +0700

    Add files via upload

---
+++ Menghitung-suhu/src/test/java/com/example/Menghitung/suhu/MenghitungSuhuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Menghitung.suhu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenghitungSuhuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ Menghitung-suhu/target/apidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"com.example.Menghitung.suhu","l":"CelciustoFarenheit"},{"p":"com.example.Menghitung.suhu","l":"konversiberat"},{"p":"com.example.Menghitung.suhu","l":"KonversiSuhu"},{"p":"com.example.Menghitung.suhu","l":"MenghitungSuhuApplication"}]
